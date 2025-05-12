f8b925336de48d70ad67c2dd176349f97e98f0c3
Geovane Schmitz <contato@geovanems.com.br>
2025-05-12 16:03:05 -0300

    Update frontend input limits, enhance error handling, and adjust backend deployment resources

--- start.sh
+++ start.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
 cd $(dirname "$0") || exit 1
+# for linux
+#minikube config set driver kvm2
+# for windows 
+#minikube config set driver docker
+# for mac
+# minikube config set driver hyperkit
+#minikube start
+sleep 5
+minikube start --cpus 6 --memory 10240
 
-eval "$(minicube docker-env)"
+minikube addons enable metrics-server
+minikube addons enable ingress
+eval "$(minikube docker-env --shell bash)"
 cd backend
 
-docker build -t python-backend:latest .
+minikube image build -t python-backend:latest .
 cd ../frontend
 
-docker build -t static-frontend:latest .
+minikube image build -t static-frontend:latest .
 
 cd ..
 # re deploy the images to kubernetes
